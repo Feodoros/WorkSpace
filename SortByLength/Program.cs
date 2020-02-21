055a8fe093b6a4653434d2a03f854e18a55789b7
Fedor Zhilkin <fzhilkin@mail.ru>
2020-02-21 22:22:23 +0300

    Add Sort 2d arrays.

---
+++ SortByLength/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace SortByLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arrays = new[]
+            {
+                new int[] {289, 200, 101, 90, 230}, new int[] {100, 289, 200, 101},
+                new int[] {100, 289, 205, 101, 90, 230}, new int[] {100, 289, 200, 101, 90, 230}
+            };
+
+            Sort(arrays);
+            
+            Console.WriteLine(arrays);
+        }
+        
+        
+        private static void Sort(int[][] arrays)
+        {
+            for (int i = 0; i < arrays.Length - 1; i++)
+            {
+                if (arrays[i].Length > arrays[i + 1].Length)
+                {
+                    int[] tempArr = arrays[i+1];
+                    arrays[i + 1] = arrays[i];
+                    arrays[i] = tempArr;
+                    Sort(arrays);
+                }
+
+                if (arrays[i].Length == arrays[i + 1].Length)
+                {
+                    if (arrays[i].Sum() < arrays[i + 1].Sum())
+                    {
+                        int[] tempArr = arrays[i+1];
+                        arrays[i + 1] = arrays[i];
+                        arrays[i] = tempArr;
+                        Sort(arrays);
+                    }
+                }
+            }
+        }
+        
+        // ДЗ: реализовать сортировку вставками/слиянием (Merge Sort)
+        // И выложить на GitHub
+    }
+}

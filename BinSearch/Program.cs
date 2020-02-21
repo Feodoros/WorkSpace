00ed2a2af9f7d3f5620351af10426eba05e04d21
Fedor Zhilkin <fzhilkin@mail.ru>
2020-02-21 18:45:28 +0300

    Add Directory Depth and BinSearch

---
+++ BinSearch/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace BinSearch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            int[,] arrays = { {-15, -12, -9, -2 }, { -1, 0, 4, 8 }, { 9, 15, 18, 22 }};
+            int X = 22;
+            
+            Print(arrays);
+            
+            
+            if (Finder_2D(arrays, X, out int[] indexI))
+            {
+                Console.WriteLine($"Число {X} имеет координаты {indexI[0]},{indexI[1]}");
+            }
+            else
+            {
+                Console.WriteLine("Число не найдено.");
+            }
+            Console.ReadKey();
+        }
+
+        private static bool Finder_2D(int[,] arrays, int X, out int[] indexI) 
+        {
+            indexI = new int[] {-1, -1};
+            // Тот же бин поиск по массиву, только теперь еще пробегаем каждую строку (массив)
+            // Ну и индекс у нас теперь двумерный
+            for (int i = 0; i <= arrays.GetUpperBound(0); i++)
+            {
+                int left, right, mid;
+                left = 0;
+                right = arrays.GetUpperBound(1);
+                
+                while (left <= right)
+                {
+                    mid = (left + right) / 2;
+                    if (arrays[i, mid] == X)
+                    {
+                        indexI[0] = i;
+                        indexI[1] = mid;
+                        return true;
+                    }
+
+                    if (arrays[i, mid] < X)
+                        left = mid + 1;
+                    else
+                        right = mid - 1;
+                }
+            }
+            return false;
+        }
+
+        private static void Print(int[,] arrays)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i <= arrays.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arrays.GetUpperBound(1); j++)
+                {
+                    Console.Write(arrays[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ DirectoryDeepth/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryDeepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тестовый путь.
+            GoDeep(@"C:\Users\Fedor\Documents\Военная кафедра");
+            
+            // Тут сортируем все полученные пути папок, берем первый (самый длинный путь)
+            // И сплитим его по '\\' чтобы получить число вложенности
+            var mostDeepDirectory = _directoriesList.Cast<string>().OrderBy(s => s.Split('\\').Length).Last();
+            var depth = mostDeepDirectory.Split(Path.DirectorySeparatorChar).Length;
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Папка: {mostDeepDirectory} \n Глубина: {depth}");
+        }
+        
+        
+        
+        // Список всех папок 
+        private static ArrayList _directoriesList = new ArrayList();
+
+        // Идем в каждую папку рекурсивно и добавляем все пути папок из этой папки в список путей
+        // Далее для каждой новой папки действия повторяются
+        // Грубо говоря, спускаемся вниз попутно добавляя папки в список
+        private static void GoDeep(string path)
+        {   
+            string[] directories = Directory.GetDirectories(path);
+            // Если в папке нет папок, то возвращаемся 
+            if (directories.Length <= 0) return;
+            foreach (string dir in directories)
+            {
+                _directoriesList.Add(dir);
+                GoDeep(dir);
+            }
+        }
+    }
+}

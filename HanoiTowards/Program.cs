1c4ecbbd00044305365c6e309c15e08691e637bb
Fedor Zhilkin <fzhilkin@mail.ru>
2020-03-27 01:03:18 +0300

    Hanoi Towards

---
+++ HanoiTowards/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HanoiTowards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi(4, 'A', 'B', 'C');
+        }
+
+        static void Hanoi(Int32 n, char from, char to, char temp)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(from + "->" + to);
+                return;
+            }
+            
+            Hanoi(n - 1, from, temp, to);
+            Console.WriteLine(from + "->" + to);
+            Hanoi(n - 1, temp, to, from);
+        }
+    }
+}

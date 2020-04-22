c999c703c066425b44f9aca32a85ff74814b0495
Fedor Zhilkin <fzhilkin@mail.ru>
2020-04-22 18:21:59 +0300

    Initial SearchFile

---
+++ HashSearchFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HashSearchFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    
+}

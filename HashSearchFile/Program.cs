b5d976d9a100af434238abf674394a29c55c7310
Fedor Zhilkin <fzhilkin@mail.ru>
2020-04-22 19:29:16 +0300

    GetHash()

--- HashSearchFile/Program.cs
+++ HashSearchFile/Program.cs
@@ -9,20 +9,66 @@ namespace HashSearchFile
     {
         static void Main(string[] args)
         {
-           var s =  GetFilesFromDirectory(@"C:\Users\Fedor\Desktop\Test");
+           var s =  @"C:\Users\Fedor\Desktop\Test";
 
-           
+           int[] s1 = new[] {1, 2, 3, 4};
+           int[] s2 = new[] {5, 6, 7, 8};
+           int[] res = s1.Concat(s2).ToArray();
            // Имя файла по пути
-           var x = Path.GetFileName(s.First());
+           //var x = Path.GetFileName();
+           
+           // Все файлы
+           var y = GetAllFiles(s);
+           
            
            
            Console.WriteLine(s);
         }
-        
-        // Получаем пути файлов в одной папке
-        public static string[] GetFilesFromDirectory(string path)
+
+        public static byte GetHashCode(string strFile)
         {
-            return Directory.GetFiles(path);
+            uint num = 0;
+            for (int index = 0; index < strFile.Length; ++index)
+                num += strFile[index];
+            return (byte)(num % 256U);
+        }
+        
+        // Получаем хэш-код от имени файла
+        public static byte GetHash(string fileName)
+        {
+            uint code = 0; 
+            foreach (var letter in fileName)
+            {
+                code += letter;
+            }
+            return (byte) (code % 256U);
+        }
+
+        // Получаем все файлы во всех каталогах
+        public static string[] GetAllFiles(string path)
+        {
+            return RecSearchFiles(path, new string[]{});
+        }
+
+        // Рекурсивный поиск файлов
+        public static string[] RecSearchFiles(string path, string[] paths)
+        {
+            // Список файлов в папке
+            string[] files = Directory.GetFiles(path);
+
+            paths = paths.Concat(files).ToArray();
+
+            // Список папок в папке
+            string[] directories = Directory.GetDirectories(path);
+            
+            if (directories.Length != 0)
+            {
+                foreach (var directory in directories)
+                {
+                    return RecSearchFiles(directory, paths);
+                }
+            }
+            return paths;
         }
     }
 }

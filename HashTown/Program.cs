9757adbd82b877cd191b739793e3fdb587c42ec7
Fedor Zhilkin <fzhilkin@mail.ru>
2020-04-29 16:39:36 +0300

    HashTown

---
+++ HashTown/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HashTown
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] arrTowns = {"Люберцы", "Город 2"};
+            
+            string[] hashTable = MakeHashTable(arrTowns).Item1;
+            int count = MakeHashTable(arrTowns).Item2;
+
+            Console.WriteLine();
+        }
+
+        public static int GetHash(string strS)
+        {
+            int res = 0;
+            foreach (char c in strS)
+            {
+                res += c;
+            }
+            res = res % 256;
+
+            return res;
+        }
+
+        public static (string[], int) MakeHashTable(string[] arrTowns)
+        {
+            int[] arrShifts = { 2, 5, 6 };
+            int count = 0;
+
+            string[] hashTable = new string[256];
+            foreach (string town in arrTowns)
+            {
+                int hashCode = GetHash(town.ToUpper());
+
+                if (hashTable[hashCode] == null)
+                    hashTable[hashCode] = town.ToUpper();
+                else
+                {
+                    hashCode += arrShifts[count % 3];
+
+                    while (hashTable[hashCode % 256] != null)
+                    {
+                        hashCode += arrShifts[count % 3];
+                        count++;
+                    }
+
+                    hashTable[hashCode%256] = town.ToUpper();
+                    count++;
+                }
+            }
+
+            return (hashTable, count);
+        }
+    }
+}

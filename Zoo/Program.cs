5d75d4fd6f5a9eec0c0e6fa95583dac9a274e1e6
Fedor Zhilkin <fzhilkin@mail.ru>
2020-05-17 13:49:39 +0300

    Add Zoo project

---
+++ Zoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

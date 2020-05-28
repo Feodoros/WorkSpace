760304a272b6309c71ea6fd26c8ffc0e664a5819
Fedor Zhilkin <fzhilkin@mail.ru>
2020-05-28 21:04:48 +0300

    Regex check

--- ContainsMaxSubString/Program.cs
+++ ContainsMaxSubString/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ContainsMaxSubString
 {
@@ -7,6 +11,26 @@ namespace ContainsMaxSubString
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string text = "abcbbbcfvbcc";
+
+            string pattern = "ab*cf*bc";
+
+            bool check = RegexContainsSubString(pattern, text);
+
+            bool check1 = ListContainsSubString(pattern, text);
+
         }
+
+        // Решение с помощью Regex 
+        private static bool RegexContainsSubString(string pattern, string text)
+        {
+            string replace = pattern.Replace("*", "\\D*");
+            Regex regex = new Regex(replace);
+            MatchCollection matches = regex.Matches(text);
+            return matches.Count != 0;
+        }
+        
+        
+       
     }
 }
